fe2918df7ed0b5d1e9d2cdccaadfb604c672a6b9
AissaniMourad <aissanimourad444@gmail.com>
2026-07-11 16:15:10 +0100

    Fix password column length

--- website/models.py
+++ website/models.py
@@ -7,7 +7,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     first_name = db.Column(db.String(150))
-    password = db.Column(db.String(150))
+    password = db.Column(db.String(256))
     transactions = db.relationship('Transaction')
 
 
